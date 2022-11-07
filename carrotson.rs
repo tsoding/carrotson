fbca7b231c8174fbb2fc89b8bd55c77c189fc28e
rexim <reximkut@gmail.com>
2022-11-08 02:15:25 +0700

    Improve memory usage. Implement save/load

--- carrotson.rs
+++ carrotson.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::time::SystemTime;
 use std::fs;
+use std::io;
 use std::env;
 use std::process::exit;
 
@@ -26,43 +27,85 @@ impl LCG {
 
 #[derive(Debug)]
 struct Freq {
-    tokens: [u16; 256],
+    tokens: Vec<(u8, u32)>,
+}
+
+fn read_u8(r: &mut impl io::Read) -> io::Result<u8> {
+    let mut buf = [0; 1];
+    r.read_exact(&mut buf)?;
+    Ok(u8::from_le_bytes(buf))
+}
+
+fn read_u32(r: &mut impl io::Read) -> io::Result<u32> {
+    let mut buf = [0; 4];
+    r.read_exact(&mut buf)?;
+    Ok(u32::from_le_bytes(buf))
+}
+
+fn read_u64(r: &mut impl io::Read) -> io::Result<u64> {
+    let mut buf = [0; 8];
+    r.read_exact(&mut buf)?;
+    Ok(u64::from_le_bytes(buf))
 }
 
 impl Freq {
+    fn branching(&self) -> usize {
+        return self.tokens.len();
+    }
+
     fn new() -> Self {
-        Self { tokens: [0; 256] }
+        Self { tokens: Vec::new() }
     }
 
     fn push(&mut self, x: u8) {
-        if self.tokens[x as usize] < u16::MAX {
-            self.tokens[x as usize] += 1;
-        } else {
-            for i in 0..self.tokens.len() {
-                if i != x as usize && self.tokens[i] > 0 {
-                    self.tokens[i] -= 1;
-                }
+        let mut found = false;
+        for (y, p) in self.tokens.iter_mut() {
+            if *y == x {
+                *p += 1;
+                found = true;
+                break;
             }
+        }
+
+        if !found {
+            self.tokens.push((x, 1))
         }
     }
 
     fn random(&self, lcg: &mut LCG) -> Option<u8> {
-        let mut sum: usize = 0;
-        for t in self.tokens.iter() {
-            sum += *t as usize;
-        }
+        let sum: usize = self.tokens.iter().map(|(_, p)| *p as usize).sum();
 
         if sum > 0 {
             let index = (lcg.random_u32() as usize)%sum;
             let mut psum: usize = 0;
-            for i in 0..self.tokens.len() {
-                psum += self.tokens[i] as usize;
+            for (y, p) in self.tokens.iter() {
+                psum += *p as usize;
                 if psum > index {
-                    return Some(i as u8)
+                    return Some(*y)
                 }
             }
         }
         None
+    }
+
+    fn write_to(&self, w: &mut impl io::Write) -> io::Result<()> {
+        w.write_all(&(self.tokens.len() as u8).to_le_bytes())?;
+        for (x, p) in self.tokens.iter() {
+            w.write_all(&x.to_le_bytes())?;
+            w.write_all(&p.to_le_bytes())?;
+        }
+        Ok(())
+    }
+
+    fn read_from(r: &mut impl io::Read) -> io::Result<Self> {
+        let mut result = Self::new();
+        let count = read_u8(r)?;
+        for _ in 0..count {
+            let x = read_u8(r)?;
+            let p = read_u32(r)?;
+            result.tokens.push((x, p));
+        }
+        Ok(result)
     }
 }
 
@@ -91,6 +134,31 @@ impl Model {
                 self.model.insert(context, freq);
             }
         }
+    }
+
+    fn write_to(&self, w: &mut impl io::Write) -> io::Result<()> {
+        w.write_all(&(self.model.len() as u64).to_le_bytes())?;
+        for (context, freq) in self.model.iter() {
+            w.write_all(&context.to_le_bytes())?;
+            freq.write_to(w)?;
+        }
+        w.flush()?;
+        Ok(())
+    }
+
+    fn read_from(r: &mut impl io::Read) -> io::Result<Self> {
+        let mut result = Self::new();
+        let count = read_u64(r)?;
+        result.model.reserve(count as usize);
+        println!("------------------------------");
+        println!("Size of the model {count}");
+        println!("------------------------------");
+        for _ in 0..count {
+            let context = read_u64(r)?;
+            let freq = Freq::read_from(r)?;
+            result.model.insert(context, freq);
+        }
+        Ok(result)
     }
 }
 
@@ -130,8 +198,9 @@ fn context_push(context: &mut u64, x: u8) {
 fn usage(program: &str) {
     eprintln!("Usage: {program} <SUBCOMMANDS> [OPTIONS]");
     eprintln!("Subcommands:");
-    eprintln!("    gen <FILES...> [-l <LIMIT>]    generate random text based on a model trained from <FILES...>");
-    eprintln!("    stats <FILES...>               print some stats of the model that is trained from <FILES...>");
+    eprintln!("    train <INPUT> <OUTPUT>     generate binary model file <OUTPUT> based on <INPUT>");
+    eprintln!("    gen <FILE> [-l <LIMIT>]    generate random text based on a model trained from <FILE>");
+    eprintln!("    stats <FILE>               print some stats of the model that is trained from <FILE>");
 }
 
 fn main() {
@@ -167,15 +236,17 @@ fn main() {
                 })
             }).unwrap_or(1024);
 
-            println!("Training the model...");
-            let mut model = Model::new();
-            let bytes = fs::read(&file_path).unwrap_or_else(|err| {
-                eprintln!("ERROR: could not read file {file_path}: {err}");
-                exit(1)
+            println!("Loading the model from {file_path}...");
+            let file = fs::File::open(&file_path).unwrap_or_else(|err| {
+                eprintln!("ERROR: could not read from file {file_path}: {err}");
+                exit(1);
             });
-            for (context, next) in Slicer::new(bytes) {
-                model.push(context, next)
-            }
+            let model = Model::read_from(&mut io::BufReader::with_capacity(200*1024*1024, file)).unwrap_or_else(|err| {
+                eprintln!("ERROR: could not read from file {file_path}: {err}");
+                exit(1);
+            });
+
+            println!("Records: {}", model.model.len());
 
             println!("Generating text...");
             println!("------------------------------");
@@ -207,7 +278,50 @@ fn main() {
                 model.push(context, next)
             }
 
+            let mut max_branching = usize::MIN;
+            let mut avg_branching = 0f32;
+            for (_context, freq) in model.model.iter() {
+                let branching = freq.branching();
+                max_branching = std::cmp::max(max_branching, branching);
+                avg_branching += branching as f32;
+            }
+            avg_branching /= model.model.len() as f32;
+
             println!("Records count: {}", model.model.len());
+            println!("Maximum branching: {max_branching}");
+            println!("Average branching: {avg_branching}");
+        }
+        "train" => {
+            let input_file_path = args.next().unwrap_or_else(|| {
+                usage(&program);
+                eprintln!("ERROR: no input file is provided");
+                exit(1);
+            });
+            let output_file_path = args.next().unwrap_or_else(|| {
+                usage(&program);
+                eprintln!("ERROR: no output file is provided");
+                exit(1);
+            });
+
+            println!("Training the model...");
+            let mut model = Model::new();
+            let bytes = fs::read(&input_file_path).unwrap_or_else(|err| {
+                eprintln!("ERROR: could not read file {input_file_path}: {err}");
+                exit(1)
+            });
+            for (context, next) in Slicer::new(bytes) {
+                model.push(context, next)
+            }
+
+            println!("Saving the model to {output_file_path}...");
+            let output_file = fs::File::create(&output_file_path).unwrap_or_else(|err| {
+                eprintln!("ERROR: could not write file {output_file_path}: {err}");
+                exit(1)
+            });
+            model.write_to(&mut io::BufWriter::new(output_file)).unwrap_or_else(|err| {
+                eprintln!("ERROR: could not write file {output_file_path}: {err}");
+                exit(1)
+            });
         }
         _ => {
             usage(&program);
